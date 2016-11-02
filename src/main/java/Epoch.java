20193a99f80f7e00fc4c6740d724bf44ce76a4b8
eugenel <eugenel@liveperson.com>
2016-11-02 17:19:29 +0200

    Initial commit

---
+++ src/main/java/Epoch.java
@@ -0,0 +1,89 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Created by eugenel on 11/2/16.
+ */
+public class Epoch {
+
+    WebDriver driver;
+
+    public Epoch(WebDriver driver){
+        this.driver=driver;
+    }
+
+    public String getEpochData() {
+        WebElement clockField = driver.findElement(By.id("ecclock"));
+        return clockField.getText();
+    }
+
+    public String getGMT(){
+        WebElement fieldGMT = driver.findElement(By.id("result1"));
+        return fieldGMT.getText();
+    }
+
+    public void clickTimeStamp(){
+        WebElement bTimestamp = driver.findElement(By.xpath("//*[@id=\"ef\"]/button[1]"));
+        bTimestamp.click();
+    }
+
+    public String getTimeZone(){
+        WebElement fieldTimeZone = driver.findElement(By.xpath("//*[@id=\"result1\"]/a"));
+        return fieldTimeZone.getText();
+    }
+
+    //Get current date 01/01/2016 00:00
+    public String getDate(){
+
+        WebElement fieldYear = driver.findElement(By.id("hcinput"));
+        WebElement fieldMonth = driver.findElement(By.id("mm"));
+        WebElement fieldDay = driver.findElement(By.name("dd"));
+        WebElement fieldHour = driver.findElement(By.name("hh"));
+        WebElement fieldMin = driver.findElement(By.name("mn"));
+        WebElement fieldSec = driver.findElement(By.name("ss"));
+
+        return " Year: "+fieldYear.getAttribute("value")+
+                " Month: "+fieldMonth.getAttribute("value")+
+                " Day: "+fieldDay.getAttribute("value")+
+                " Hour: "+fieldHour.getAttribute("value")+
+                " Minutes: "+fieldMin.getAttribute("value")+
+                " Seconds: "+fieldSec.getAttribute("value");
+    }
+
+    //Clear all the data from the page
+    public void clear(){
+        WebElement link = driver.findElement(By.xpath("//*[@id=\"contentcolumn\"]/div[2]/p[3]/a"));
+        link.click();
+    }
+
+    //Set 2016 year beginning
+    public void setDate(){
+
+        WebElement fieldYear = driver.findElement(By.id("hcinput"));
+        WebElement fieldMonth = driver.findElement(By.id("mm"));
+        WebElement fieldDay = driver.findElement(By.name("dd"));
+        WebElement fieldHour = driver.findElement(By.name("hh"));
+        WebElement fieldMin = driver.findElement(By.name("mn"));
+        WebElement fieldSec = driver.findElement(By.name("ss"));
+
+        fieldYear.sendKeys("2016");
+        fieldMonth.sendKeys("01");
+        fieldDay.sendKeys("01");
+        fieldHour.sendKeys("01");
+        fieldMonth.sendKeys("00");
+        fieldMin.sendKeys("00");
+        fieldSec.sendKeys("00");
+
+    }
+
+    //Receive timestamp of the 2016 beginning
+    public String getTimeStamp(){
+        WebElement bToTimestamp = driver.findElement(By.xpath("//*[@id=\"hf\"]/table[3]/tbody/tr/td/button"));
+        WebElement Timestamp2016 = driver.findElement(By.id("result2"));
+        bToTimestamp.click();
+        return Timestamp2016.getText();
+    }
+
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+
+public class Main {
+
+    static String driverPath = "/Users/eugenel/Documents/Code/Selen3Try/";
+    static public WebDriver driver;
+
+    public static void main(String[] args) {
+
+
+        System.out.println("launching firefox browser");
+        System.setProperty("webdriver.gecko.driver", driverPath+"geckodriver");
+
+        File pathToBinary = new File("/Applications/Firefox49.app/Contents/MacOS/firefox-bin");
+        FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
+        FirefoxProfile firefoxProfile = new FirefoxProfile();
+        driver = new FirefoxDriver(ffBinary,firefoxProfile);
+        driver.navigate().to("http://www.epochconverter.com/");
+
+        waitForLoad(driver);
+
+        Epoch epoch = new Epoch(driver);
+
+        //Read the epoch time
+        System.out.println("Epoch date:" +epoch.getEpochData());
+
+        //Lets try to take GMT before it shown
+        //Read the epoch time
+        System.out.println("GMT still not shown "+ epoch.getGMT());
+
+        //Click on the converter
+        epoch.clickTimeStamp();
+
+        //Read the epoch time
+        System.out.println("Current " + epoch.getGMT());
+
+        //Read Time Zone
+        System.out.println("Your time Zone: " + epoch.getTimeZone());
+
+        //Get date
+        System.out.println("Current date: " + epoch.getDate());
+
+
+        epoch.clear();
+
+        epoch.setDate();
+
+        epoch.getTimeStamp();
+
+        System.out.println("The timestamp of 01/01/2016: " + epoch.getTimeStamp());
+
+
+
+        if(driver!=null) {
+            driver.close();
+            System.out.println("Driver closed - bye bye");
+        }
+
+    }
+
+    static void  waitForLoad(WebDriver driver) {
+        new WebDriverWait(driver, 30).until((ExpectedCondition<Boolean>) wd ->
+                ((JavascriptExecutor) wd).executeScript("return document.readyState").equals("complete"));
+    }
+}
